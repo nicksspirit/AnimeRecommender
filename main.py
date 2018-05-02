ba629b222e08dc85e796c4e9abcd8538076ddc22
Nick Spirit <nickspirit3+gitHub@gmail.com>
2018-05-02 17:38:32 +0300

    Initial commit for anime recommendation engine powered by grakn

---
+++ main.py
@@ -0,0 +1,74 @@
+from typing import Set, Tuple, Iterable
+from itertools import chain
+from tabulate import tabulate
+from math import isnan
+import csv
+import os
+import pandas as pd
+
+BASE_DIR = os.getcwd()
+CSV_DIR = os.path.join(BASE_DIR, 'anime-recommendations-csv')
+
+
+def write_to_csv(csv_file: str, heading: Tuple[str], rows: Iterable[Tuple[str]]) -> None:
+    with open(os.path.join(CSV_DIR, csv_file), 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(heading)
+
+        for row in rows:
+            writer.writerow(row)
+
+
+def clean(string) -> str:
+    # Handle NaN values returned by pandas
+    if isinstance(string, float) and isnan(float(string)):
+        return ''
+    return str(string).strip()
+
+
+def get_unique_genres(genre_df: pd.DataFrame) -> Set:
+    return {
+        clean(genre)
+        for row in genre_df
+        for genre in clean(row).split(',')
+    }
+
+
+def split_anime_genre(anime_id: str, genre: str) -> Iterable:
+    genre_list = map(lambda g: clean(g), clean(genre).split(','))
+    return map(lambda genre: (anime_id, genre), genre_list)
+
+
+def prep_user_data(user_df: pd.DataFrame) -> None:
+    unique_users = {user_id for user_id in user_df}
+
+    user_ids = map(lambda id: (id, 'anonymous'), unique_users)
+
+    write_to_csv('user.csv', ('user_id', 'name'), user_ids)
+
+
+def prep_genre_data(genre_df: pd.DataFrame) -> None:
+    # Remove empty string (nan) values
+    genre_df.dropna(how='all', inplace=True)
+    rows_of_genres = map(lambda genre: (genre,), get_unique_genres(genre_df))
+    write_to_csv('genre.csv', ('name',), rows_of_genres)
+
+
+def prep_anime_genre_data(anime_id_df: pd.DataFrame, genre_df: pd.DataFrame) -> None:
+    zipped_df = zip(anime_id_df, genre_df)
+
+    anime_id_genre = [
+        split_anime_genre(anime_id, genre)
+        for anime_id, genre in zipped_df
+    ]
+
+    write_to_csv('anime_genre.csv', ('anime_id', 'genre'), chain.from_iterable(anime_id_genre))
+
+
+if __name__ == '__main__':
+    anime_df = pd.read_csv(os.path.join(CSV_DIR, 'anime.csv'))
+    user_rating_df = pd.read_csv(os.path.join(CSV_DIR, 'rating.csv'))
+
+    prep_genre_data(anime_df['genre'])
+    prep_anime_genre_data(anime_df['anime_id'], anime_df['genre'])
+    prep_user_data(user_rating_df['user_id'])
