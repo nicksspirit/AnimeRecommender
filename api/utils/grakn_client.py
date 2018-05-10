03c9a3ed724c3e93791f1b12bfa8ce4e391e07da
Nick Spirit <nickspirit3+gitHub@gmail.com>
2018-05-11 01:57:05 +0300

    pep8 formatting

--- api/utils/grakn_client.py
+++ api/utils/grakn_client.py
@@ -1,15 +1,13 @@
 import grakn
 from api import settings
-from typing import Tuple
+
 
 class Grakn():
     def __init__(self, uri=settings.GRAKN_URL, keyspace=settings.GRAKN_KEYSPACE):
         self.client = grakn.Client(
-                uri=uri,
-                keyspace=keyspace
-            )
+            uri=uri,
+            keyspace=keyspace,
+        )
 
     def query(self, query: str):
         return self.client.execute(query)
-
-
