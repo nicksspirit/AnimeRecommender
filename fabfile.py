ec6eb437a66fd977f467ce1aa4ac34ddc193a1e3
Nick Spirit <nickspirit3+gitHub@gmail.com>
2018-05-23 15:50:59 +0300

    Added commands to start, stop and reset grakn

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,26 @@
 from fabric.api import local
 
 
+def grakn_clean():
+    '''Clean Grakn database'''
+    local('grakn server clean')
+
+
+def grakn_stop():
+    local('grakn server stop')
+
+
+def grakn_start():
+    local('grakn server start')
+
+
+def grakn_factory_reset():
+    '''Clean and restart Grakn'''
+    grakn_stop()
+    grakn_clean()
+    grakn_start()
+
+
 def dev():
     '''Start gunicorn and watch for changes'''
     local('gunicorn --reload api:api')
